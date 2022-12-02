bcc3e386096d4fbf31e2ed0e554b8bd355f5d7bb
ShuyingHan <118194391+ShuyingHan@users.noreply.github.com>
2022-12-02 16:38:33 -0500

    add initial file

---
+++ index.js
@@ -0,0 +1,8 @@
+function mao() {
+    window.location.href = "https://www.google.com/maps";
+  }
+  function sear() {
+    console.log(document.getElementsByClassName("123")[0]);
+    document.getElementsByClassName("123")[0].className = "123 seblock";;
+  }
+  
